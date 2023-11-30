dbb8f23a50146c59fc903d1fe51c2fea673a043d
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-30 10:57:00 +0700

    Added get coupon

---
+++ src/MSL.Application/Features/Coupon/Queries/GetAllCoupon/GetAllCouponQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using MLS.Application.Contracts.Persistence;
+using MLS.Application.DTO.Coupon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLS.Application.Features.Coupon.Queries.GetAllCoupon
+{
+    public class GetAllCouponQueryHandler : IRequestHandler<GetAllCouponQuery, List<CouponDto>>
+    {
+        private readonly ICouponRepository _couponRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllCouponQueryHandler(ICouponRepository couponRepository, IMapper mapper)
+        {
+            _couponRepository = couponRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CouponDto>> Handle(GetAllCouponQuery request, CancellationToken cancellationToken)
+        {
+            var coupons = await _couponRepository.GetAll();
+            var data = _mapper.Map<List<CouponDto>>(coupons);
+
+            return data;
+        }
+    }
+}
---
+++ src/MSL.Application/Features/Coupon/Queries/GetCouponDetail/GetCouponDetailQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using MLS.Application.Contracts.Persistence;
+using MLS.Application.DTO.Coupon;
+using MLS.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLS.Application.Features.Coupon.Queries.GetCouponDetail
+{
+    public class GetCouponDetailQueryHandler : IRequestHandler<GetCouponDetailQuery, CouponDetailDto>
+    {
+        private readonly ICouponRepository _couponRepository;
+        private readonly IMapper _mapper;
+
+        public GetCouponDetailQueryHandler(ICouponRepository couponRepository, IMapper mapper)
+        {
+            _couponRepository = couponRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponDetailDto> Handle(GetCouponDetailQuery request, CancellationToken cancellationToken)
+        {
+            var coupon = await _couponRepository.GetById(request.Id);
+
+            if (coupon == null)
+            {
+                throw new NotFoundException(nameof(Domain.Coupon), request.Id);
+            }
+
+            var data = _mapper.Map<CouponDetailDto>(coupon);
+
+            return data;
+        }
+    }
+}
